b1fcdd9cd14e20dc2311fe22402c3401dc35b11c
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-06-21 11:22:14 +0300

    refactoring

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,53 +2,10 @@ package repository
 
 import (
 	models "metricapp/internal/model"
-	"sync"
 )
 
 type Repo interface {
 	SetField(string, models.Metrics)
 	GetFields() map[string]models.Metrics
 	IncrementCounter()
-}
-
-type InMemoryStorage struct {
-	metrics map[string]models.Metrics
-	mu      sync.RWMutex
-}
-
-func NewInMemoryStorage() *InMemoryStorage {
-	return &InMemoryStorage{
-		metrics: make(map[string]models.Metrics),
-	}
-}
-
-func (s *InMemoryStorage) SetField(key string, value models.Metrics) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.metrics[key] = value
-}
-
-func (s *InMemoryStorage) GetFields() map[string]models.Metrics {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
-	// Копируем мапу
-	newMap := make(map[string]models.Metrics)
-	for k, v := range s.metrics {
-		newMap[k] = v
-	}
-
-	return newMap
-}
-
-func (s *InMemoryStorage) IncrementCounter() {
-	pollCounter := s.metrics["PollCounter"]
-	if pollCounter.Delta == nil {
-		var zeroCounter int64
-		pollCounter.Delta = &zeroCounter
-	}
-
-	newCounterValue := *pollCounter.Delta + 1
-	pollCounter.Delta = &newCounterValue
-	s.metrics["PollCounter"] = pollCounter
 }
