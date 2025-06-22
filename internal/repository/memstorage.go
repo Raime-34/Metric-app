1e7ae862d1d0d1df27dd52f625e3494dc20d4528
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-06-22 13:57:41 +0300

    add memstorage

---
+++ internal/repository/memstorage.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type MemStorage struct {
+	storage map[string]float64
+	counter atomic.Int64
+	mu      sync.RWMutex
+}
+
+func NewMemStorage() MemStorage {
+	return MemStorage{
+		storage: make(map[string]float64),
+	}
+}
+
+func (ms *MemStorage) SetField(key string, value float64) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+	ms.storage[key] = value
+}
+
+func (ms *MemStorage) GetFields() map[string]float64 {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
+	newMap := make(map[string]float64)
+	for key, value := range ms.storage {
+		newMap[key] = value
+	}
+
+	return newMap
+}
+
+func (ms *MemStorage) IncrementCounter(n ...int64) {
+	if len(n) == 0 {
+		return
+	}
+
+	ms.counter.Add(n[0])
+}
---
+++ internal/repository/memstorage_test.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemStorage_SetField(t *testing.T) {
+	storage := NewMemStorage()
+
+	key := "Test"
+	var expected float64 = 5
+	storage.SetField(key, expected)
+	actual := storage.GetFields()[key]
+	assert.Equal(t, expected, actual)
+}
+
+func TestMemStorage_IncrementCounter(t *testing.T) {
+	storage := NewMemStorage()
+
+	var inc int64 = 10
+	storage.IncrementCounter(inc)
+
+	assert.Equal(t, inc, storage.counter.Load())
+}
