455797ed97ed44022d756890f26c590fd097571a
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-06-24 18:56:40 +0300

    add flag parse to main

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"flag"
 	"metricapp/internal/agent"
 	"metricapp/internal/logger"
 )
 
 func main() {
+	flag.Parse()
 	logger.InitLogger()
 
 	logger.Info("Starting metrics collection")
