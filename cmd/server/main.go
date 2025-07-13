19901d56cc3d389b09d07cae6ddebf01acad44d7
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-07-13 10:09:05 +0300

    handler -> server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"metricapp/internal/handler"
+	"metricapp/internal/server"
 )
 
 func main() {
-	handler.Start()
+	server.Start()
 }
--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,4 +1,4 @@
-package handler
+package server
 
 import (
 	models "metricapp/internal/model"
