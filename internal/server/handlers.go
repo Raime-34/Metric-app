ff158ec844c769d42715c0c0b068e047a4c9caeb
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-07-13 12:36:41 +0300

    upd get metric response format

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -69,7 +69,7 @@ func (h *MetricHandler) GetMetric(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		s := strconv.FormatFloat(v, 'f', 3, 64)
+		s := strconv.FormatFloat(v, 'f', 1, 64)
 		w.Write([]byte(s))
 	case models.Counter:
 		counter, ok := h.storage.GetCounter(mName)
