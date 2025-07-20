72fdef7bb8e5b902377e9fb770da89d8726e62da
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-07-20 13:37:46 +0300

    upd filed name

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -151,7 +151,7 @@ func (h *MetricHandler) UpdateMetricsWJSONv2(w http.ResponseWriter, r *http.Requ
 		var cMetrics struct {
 			ID    string `json:"id"`
 			Type  string `json:"type"`
-			Value int64  `json:"value"`
+			Value int64  `json:"delta"`
 		}
 
 		err := json.Unmarshal(b, &cMetrics)
