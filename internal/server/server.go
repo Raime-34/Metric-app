a03d2265fe7f828318db1ed7b0b13911d6033d8d
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-07-20 20:42:34 +0300

    fxd encoding

--- internal/server/server.go
+++ internal/server/server.go
@@ -78,8 +78,18 @@ func (r *loggingResponseWriter) WriteHeader(statusCode int) {
 	r.responseData.status = statusCode
 }
 
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	Writer *gzip.Writer
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
 func gzipHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Распаковка тела запроса, если оно в gzip
 		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
 			gz, err := gzip.NewReader(r.Body)
 			if err != nil {
@@ -87,9 +97,20 @@ func gzipHandler(next http.Handler) http.Handler {
 				return
 			}
 			defer gz.Close()
-
 			r.Body = gz
 		}
+
+		// Обработка gzip-ответа, если клиент поддерживает
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Set("Content-Encoding", "gzip")
+			gz := gzip.NewWriter(w)
+			defer gz.Close()
+			grw := gzipResponseWriter{ResponseWriter: w, Writer: gz}
+			next.ServeHTTP(grw, r)
+			return
+		}
+
+		// Если клиент не поддерживает gzip
 		next.ServeHTTP(w, r)
 	})
 }
