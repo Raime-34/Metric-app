b4d6f2442b558724c273a94c283e4318c42d97b8
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-08-07 14:55:46 +0300

    add go-migrate support

--- internal/repository/psql_handler.go
+++ internal/repository/psql_handler.go
@@ -2,11 +2,16 @@ package repository
 
 import (
 	"context"
+	"database/sql"
+	"log"
 	"metricapp/internal/logger"
 	"sync"
 	"time"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"go.uber.org/zap"
 )
 
@@ -16,26 +21,56 @@ var (
 )
 
 type PsqlHandler struct {
-	conn *pgx.Conn
+	dbClient *sql.DB
 }
 
 func NewPsqlHandler(dsn string) {
 	once.Do(func() {
-		conn, err := pgx.Connect(context.Background(), dsn)
+		db, err := sql.Open("pgx", dsn)
 		if err != nil {
 			logger.Error("failed to connect to db", zap.Error(err))
 			return
 		}
 
 		psqlHandler = &PsqlHandler{
-			conn: conn,
+			dbClient: db,
+		}
+
+		err = migration()
+		if err != nil {
+			log.Fatalf("failed to make migration: %v", err)
 		}
 	})
+}
+
+func migration() error {
+	log.Println(psqlHandler.dbClient)
+	conn, err := psqlHandler.dbClient.Conn(context.Background())
+	if err != nil {
+		return err
+	}
+
+	driver, err := postgres.WithConnection(context.Background(), conn, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file:///app/migrations", "postgres", driver)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func Ping() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	return psqlHandler.conn.Ping(ctx)
+	return psqlHandler.dbClient.PingContext(ctx)
 }
