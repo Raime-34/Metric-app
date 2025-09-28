28e940ddc53d2bd5dbf6cb58cdf74366d9a112d1
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2025-09-28 23:13:17 +0300

    gewgwe

--- internal/server/cfg/config.go
+++ internal/server/cfg/config.go
@@ -37,9 +37,10 @@ func LoadConfig() {
 	if Cfg.MigrationPath == "" {
 		flag.StringVar(&Cfg.MigrationPath, "m", "migrations", "Путь к фалам миграции")
 	}
-	if Cfg.Key == "" {
-		flag.StringVar(&Cfg.Key, "k", "", "Ключ для хэширования")
-	}
+	fmt.Println(Cfg.Key)
+	// if Cfg.Key == "" {
+	flag.StringVar(&Cfg.Key, "k", "", "Ключ для хэширования")
+	// }
 	fmt.Println(Cfg.Key)
 	var restore bool
 	flag.BoolVar(&restore, "r", false, "Флаг для загрузки сохраненных метрик с предыдущего сеанса")
